1cb6dd48de093fa2624d3822a1fe8b59ce0d0634
aihe <379879419@qq.com>
2020-04-27 21:53:05 +0800

    Update sk7_normalization.py

--- sklearnTUT/sk7_normalization.py
+++ sklearnTUT/sk7_normalization.py
@@ -14,22 +14,39 @@ from sklearn.datasets.samples_generator import make_classification
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 
+# eg1
 a = np.array([[10, 2.7, 3.6],
                      [-100, 5, -2],
                      [120, 20, 40]], dtype=np.float64)
 print(a)
+# 归一化 默认的方式
 print(preprocessing.scale(a))
+# 归一化 minmax_scale方式
+# print(preprocessing.minmax_scale(a, feature_range=(-5,5)))
 
+
+# eg2
 X, y = make_classification(n_samples=300, n_features=2 , n_redundant=0, n_informative=2,
                            random_state=22, n_clusters_per_class=1, scale=100)
+
+# X 300个样本，每个样本2维，y为类别标签，这里是0或者1
+# 分析生成的数据
 plt.scatter(X[:, 0], X[:, 1], c=y)
+print(type(X))
+print(X.dtype)
+print(X.shape)
+print(X[:, 0])
+print(X[:, 0])
+print(y)
+
+
 plt.show()
 X = preprocessing.scale(X)    # normalization step
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
 clf = SVC()
 clf.fit(X_train, y_train)
+print()
 print(clf.score(X_test, y_test))
-
 
 
 
